f46a201601242477bd095435e8f7b70d0863345c
srl23 <srl23ga@gmail.com>
2021-06-18 15:05:16 -0400

    purchasing things now works and adds to income

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let showCount = document.getElementById("counter");
-let count = 0;
+let houseCount = document.getElementById("houses-owned");
+let money = 0;
+let house = 0;
+let income = 1;
+
+let houseValue = 1;
 
 function countUp() {
-    count += 1;
-    console.log(count);
+    money += income;
     updateViews();
+}
+
+function buyHouse() {
+    if(money >= 1) {
+        money -= 1;
+        income += houseValue;
+        house++;
+        console.log(house);
+        houseCount.innerHTML = "Houses: " + house;
+        updateViews();
+    } else {
+        //nothing
+    }
 }
 
 let interval = setInterval(countUp, 1000);
 
 function updateViews() {
-    showCount.innerHTML = count;
+    showCount.innerHTML = "Money: " + money;
 }
