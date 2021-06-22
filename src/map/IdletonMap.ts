b118e4340ba9010b44f80bfce4406862f7b63e89
Chris Hampel <bWGi.panther@gmail.com>
2021-06-22 17:23:19 -0400

    refactor to clean up long lines

--- src/map/IdletonMap.ts
+++ src/map/IdletonMap.ts
@@ -5,11 +5,15 @@ export class IdletonMap {
     map: Building[][];
     x_size: number;
     y_size: number;
+    cell_width: number
+    cell_height: number
 
     constructor(canvas: CanvasRenderingContext2D, x_size: number = 20, y_size: number = 20) {
         this.canvas = canvas;
         this.x_size = x_size
         this.y_size = y_size
+        this.cell_width = this.canvas.canvas.width / this.x_size;
+        this.cell_height = this.canvas.canvas.height / this.y_size;
         this.map = new Array(this.x_size)
         for (let x = 0; x < x_size; x++) {
             this.map[x] = new Array(this.y_size)
@@ -28,7 +32,7 @@ export class IdletonMap {
         this.canvas.clearRect(0, 0, this.canvas.canvas.width, this.canvas.canvas.height);
         this.map.forEach((row, x) => {
             row.forEach((building, y) => {
-                building?.render(time, x, y, this.canvas.canvas.width / this.x_size, this.canvas.canvas.height / this.y_size)
+                building?.render(time, x, y, this.cell_width, this.cell_height)
             });
         });
     }
