7b159d06258521b0da5ede40b0dbc742ead8f783
Chris Hampel <bWGi.panther@gmail.com>
2021-06-22 17:58:32 -0400

    allow rendering only part of the canvas

--- src/buildings/building.ts
+++ src/buildings/building.ts
@@ -4,6 +4,6 @@ export interface Building {
     map: IdletonMap;
     canvas: CanvasRenderingContext2D;
     name(): string;
-    update(time_diff: number): null;
-    render(time: number, x,y, width, height): null;
+    update(time_diff: number,x: number,y: number ): null;
+    render(time: number, x: number,y: number, width: number, height: number): null;
 }
--- src/map/IdletonMap.ts
+++ src/map/IdletonMap.ts
@@ -5,15 +5,25 @@ export class IdletonMap {
     map: Building[][];
     x_size: number;
     y_size: number;
-    cell_width: number
-    cell_height: number
+    offset_y_top: number = 0;
+    offset_y_bottom: number = 0;
+    offset_x_left: number = 0;
+    offset_x_right: number = 0;
 
-    constructor(canvas: CanvasRenderingContext2D, x_size: number = 20, y_size: number = 20) {
+    constructor(canvas: CanvasRenderingContext2D,
+                offset_y_top: number = 0,
+                offset_y_bottom: number = 0,
+                offset_x_left: number = 0,
+                offset_x_right: number = 0,
+                x_size: number = 20,
+                y_size: number = 20) {
         this.canvas = canvas;
         this.x_size = x_size
         this.y_size = y_size
-        this.cell_width = this.canvas.canvas.width / this.x_size;
-        this.cell_height = this.canvas.canvas.height / this.y_size;
+        this.offset_y_top = offset_y_top
+        this.offset_y_bottom = offset_y_bottom
+        this.offset_x_left = offset_x_left
+        this.offset_x_right = offset_x_right
         this.map = new Array(this.x_size)
         for (let x = 0; x < x_size; x++) {
             this.map[x] = new Array(this.y_size)
@@ -21,19 +31,36 @@ export class IdletonMap {
     }
 
     update(time_diff: number) {
-        for (let row of this.map) {
-            for (let building of row) {
-                building?.update(time_diff)
-            }
-        }
-    }
-
-    render(time: number) {
-        this.canvas.clearRect(0, 0, this.canvas.canvas.width, this.canvas.canvas.height);
         this.map.forEach((row, x) => {
             row.forEach((building, y) => {
-                building?.render(time, x, y, this.cell_width, this.cell_height)
+                building?.update(time_diff, x, y);
             });
         });
     }
+
+    render(time: number) {
+        this.canvas.clearRect(
+            this.offset_x_left,
+            this.offset_y_top,
+            this.canvas.canvas.width - this.offset_x_right,
+            this.canvas.canvas.height - this.offset_y_bottom
+        );
+        let cell_width = this.cell_width()
+        let cell_height = this.cell_height()
+        this.map.forEach((row, x) => {
+            row.forEach((building, y) => {
+                building?.render(time, this.get_x(x), this.get_y(y), cell_width, cell_height)
+            });
+        });
+    }
+
+    private draw_height = () => this.canvas.canvas.height - this.offset_y_top - this.offset_y_bottom;
+    private draw_width = () => this.canvas.canvas.width - this.offset_x_left - this.offset_x_right;
+
+    private cell_width = () => this.draw_width() / this.x_size;
+    private cell_height = () => this.draw_height() / this.y_size;
+
+    private get_x = (x: number) => x * this.cell_width() + this.offset_x_left;
+    private get_y = (y: number) => y * this.cell_height() + this.offset_y_top;
+
 }
